48ce4ebf628898b16a03f55643fc5a3aee6f7969
Snick <nickamorg0@gmail.com>
2020-05-01 20:50:07 +0300

    Functionality to add selected namedays as events on google calendar

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 import 'package:flutter/material.dart';
+import 'dart:collection';
+import 'package:manage_calendar_events/manage_calendar_events.dart';
 
 void main() => runApp(MyApp());
 
@@ -57,6 +59,14 @@ class RandomWordsState extends State<RandomWords> {
 	final Set<NameDay> selectedNameDays = Set<NameDay>();
 	final _biggerFont = const TextStyle(fontSize: 18.0);
 	String calendarID;
+
+	RandomWordsState() {
+		CalendarPlugin deviceCalendarPlugin = new CalendarPlugin();
+
+		deviceCalendarPlugin.getCalendars().then((calendars) => 
+			calendarID = calendars.getRange(0, 1).toList()[0].id
+		);
+	}
 
   	@override
 	Widget build(BuildContext context) {
@@ -118,6 +128,7 @@ class RandomWordsState extends State<RandomWords> {
 							padding: EdgeInsets.all(16.0),
 							
 							onPressed: () => {
+								addNamedaysToCalendar()
 							},
 
 							child: Text(
@@ -168,6 +179,55 @@ class RandomWordsState extends State<RandomWords> {
 				});
 			},     
 		);
+	}
+
+	addNamedaysToCalendar() {
+		CalendarPlugin deviceCalendarPlugin = new CalendarPlugin();
+	
+		String calendarID = "2";
+
+		deviceCalendarPlugin.getCalendars().then((calendars) => {
+			calendarID = calendars.getRange(0, 1).toList()[0].id
+		});
+
+		print("CALENDAR ID " + calendarID);
+		calendarID = "2";
+
+
+		// RetrieveEventsParams retrieveEventsParams = new RetrieveEventsParams(startDate: new DateTime(new DateTime.now().year, 1, 1), endDate: new DateTime(new DateTime.now().year + 1, 12, 31));
+
+		deviceCalendarPlugin.getEvents(calendarId:"2").then((val) {
+			val.forEach((event) {
+				if (event.title.contains("🎂 Ονομαστική Εορτή: ")) {
+					print("CONTAINSSSSSSS " + event.eventId);
+					deviceCalendarPlugin.deleteEvent(calendarId: calendarID, eventId: event.eventId).then((val) {
+						print(val);
+						print("DELETED");
+						// print(val.errorMessages);
+					});
+				}
+			});
+			// deleteEvent(String calendarId, String eventId)
+		});
+
+		CalendarEvent event = new CalendarEvent();
+		
+		selectedNameDays.forEach((nameday) {
+			// event = calendarID;
+			event.title = "🎂 Ονομαστική Εορτή: " + nameday.name + " 🎂";
+			event.description = "Σήμερα γιορτάζει ο " + nameday.name + ". Ευχηθείτε του Xρόνια Πολλά.";
+
+			int year = int.parse(nameday.date.split("-")[2]);
+			int month = int.parse(nameday.date.split("-")[1]);
+			int day = int.parse(nameday.date.split("-")[0]);
+
+			event.startDate = new DateTime(year, month, day);
+			event.endDate = new DateTime(year, month, day + 1);
+			
+			event.isAllDay = true;
+			
+			deviceCalendarPlugin.createEvent(calendarId: "2", event: event);
+		});
 	}
 }
 
