d799c53c38c9e2b601a3cded556dacc6db17746a
Snick <nickamorg0@gmail.com>
2020-05-07 17:49:21 +0300

    Algorithm to calculare Easter day and namedays relative of that day

--- lib/nameday.dart
+++ lib/nameday.dart
@@ -33,10 +33,33 @@ class NameDay {
 		return newName.startsWith(substring) || newHypocorisms.startsWith(substring) || newHypocorisms.contains(", " + substring) || date.contains(substring);
 	}
 
-     factory NameDay.fromJson(Map<String, dynamic> json) {
+    factory NameDay.fromJson(Map<String, dynamic> json) {
         return new NameDay(
             name: json['name'] as String,
             date: (json['date'] as String) + "-" + new DateTime.now().year.toString(),
+            hypocorisms: json['hypocorisms'] as String,
+        );
+    }
+
+    factory NameDay.fromJsonMovable(Map<String, dynamic> json, int year, int month, int day) {
+        int milliseconds = new DateTime(year, month, day).millisecondsSinceEpoch;
+        String daysRange = json['daysRange'] as String;
+
+        if(daysRange.length == 2) {
+            if(daysRange.substring(0, 1) == '-') {
+                milliseconds -= int.parse(daysRange.substring(1)) * 24 * 60 * 60 * 1000;
+            } else {
+                milliseconds += int.parse(daysRange.substring(1)) * 24 * 60 * 60 * 1000;
+            }
+        } else {
+            milliseconds += int.parse(daysRange) * 24 * 60 * 60 * 1000;
+        }
+
+        DateTime correct = new DateTime.fromMillisecondsSinceEpoch(milliseconds);
+        
+        return new NameDay(
+            name: json['name'] as String,
+            date: correct.day.toString() + "-" + correct.month.toString() + "-" + correct.year.toString(),
             hypocorisms: json['hypocorisms'] as String,
         );
     }
@@ -44,6 +67,9 @@ class NameDay {
 
 class NameDays {
 	static List<NameDay> nameDaysList;
+    final int year = new DateTime.now().year;
+    int day;
+    int month = 4;
 
     NameDays() {
 		nameDaysList = new List<NameDay>();
@@ -58,10 +84,25 @@ class NameDays {
                 nameDaysList.add(NameDay.fromJson(nameday));
             });
 
+            calculateEasterDay();
+
+            namedays["movableNamedays"].forEach((nameday) {
+                nameDaysList.add(NameDay.fromJsonMovable(nameday, year, month, day));
+            });
+
             nameDaysList.sort((a, b) {
                 return a.name.compareTo(b.name);
             });
         });
+    }
+
+    calculateEasterDay() {
+        day  = (((19 * (year % 19) + 16) % 30) + ((2*(year % 4)+4*(year % 7) +6*((19 * (year % 19) + 16) % 30)) %7) + 3);
+        
+        if(day >= 31) {
+            day = (day - 30);
+            month = 5;
+        }
     }
 
 }
